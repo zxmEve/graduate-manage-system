62b3c0c135204eb9a4f3ba1aadf9e1264e92df76
zhouximin <zhouximin@keep.com>
2019-03-04 14:20:09 +0800

    feature:intialize project

---
+++ src/main/java/com/zxm/graduatemanagesystem/GraduateManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zxm.graduatemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduateManageSystemApplication.class, args);
+    }
+
+}
